9e4a28a84200e94a04d4fefe3cd245e45e20582c
HenryLima07 <00033721@uca.edu.sv>
2022-06-21 14:37:15 -0600

    ✨añadiendofunciones

--- Cp4Fernando00002222.cpp
+++ Cp4Fernando00002222.cpp
@@ -1,23 +1,31 @@
+
 #include <iostream>
 using namespace std;
 
+int factura(int pagofactura);
+int validacion(int numcel);
+string confirmacion(string correo, string confi_correo);
+
 int main()
 {
-
+    string correo, confi_correo;
     int pagofactura, descuentofactura, descuentofinal, Celnuevo, numcel;
     int opt;
-    do{
-        cout << " Gracias por llamar a Claro " << endl;
+    do
+    {
+        cout << " Gracias por llamar a Claro, la mejor red de El Salvador " << endl;
         cout << " Para pago de factura presione 1 " << endl;
         cout << " Para cambio de numero de telefono presione 2 " << endl;
         cout << " Para cambiar plan de celular presione 3 " << endl;
-        cout << " Para salir presione 4 " << endl;
+        cout << " Para agenda de citas presione 4 " << endl;
+        cout << " Para salir presione 5 " << endl;
         cin >> opt;
 
 
         switch (opt)
         {
         case 1:
+
             cout << "Estas llamando al departamento de pagos de facturas" << endl;
             do
             {
@@ -27,31 +35,21 @@ int main()
 
             while (pagofactura <= 0);
 
-            if (pagofactura >= 40)
-            {
-                cout << " Su monto a pagar es mayor a 40 dolares, por lo tanto se la aplicara el 15% de descuento " << endl;
-                descuentofactura = 0.15 * pagofactura;
-                descuentofinal = pagofactura - descuentofactura;
-                cout << " el valor a cancelar es de $ " << descuentofinal << endl;
-            }
-            else
-            {
-                cout << "Su monto a cancelar es menor de 40$, su total a pagar es: " << pagofactura;
-            }
+            factura(pagofactura);
 
             break;
-
-
 
 
         case 2:
+
             cout << "Estas llamando al departamento de Cambio de numero de telefono " << endl;
             cout << " Ingrese su numero de telefono para ser cambiado " << endl;
             cin >> numcel;
+
+            validacion(numcel);
+
             cout << " Su numero de telefono ha sido ingresado correctamente, pronto le enviaremos un mensaje " << endl;
             break;
-
-
 
 
         case 3:
@@ -87,7 +85,19 @@ int main()
                 cout << " El celular Huawei P40 pro ha sido agregado a su plan de renovacion  ";
             }
             break;
+
+
         case 4:
+
+            cout << " Usted esta llamando al departamento de citas " << endl;
+            cout << " Ingrese su correo para agendarle una cita";
+            cin >> correo;
+            cout << " Vuelva a ingresar su correo ";
+            cin >> confi_correo;
+            confirmacion(correo, confi_correo);
+
+
+        case 5:
             cout << "Gracias por llamar al operador de claro";
             break;
 
@@ -96,7 +106,70 @@ int main()
             cout << " La numero que marco no es valido, porfavor intente de nuevo ";
             break;
         }
-    }while(opt != 4);
+    } while (opt != 5);
 
     return 0;
+}
+
+
+
+int factura(int pagofactura)
+{
+
+    if (pagofactura >= 40)
+    {
+        cout << " Su monto a pagar es mayor a 40 dolares, por lo tanto se la aplicara el 15% de descuento " << endl;
+        pagofactura *= 0.85;
+        cout << " el valor a cancelar es de $ " << pagofactura << endl;
+    }
+    else
+    {
+        cout << "Su monto a cancelar es menor de 40$, su total a pagar es : " << pagofactura << endl;
+    }
+    return pagofactura;
+}
+
+int validacion(int numcel)
+{
+
+    bool valido = false;
+
+    do
+    {
+        if (numcel > 99999999 || numcel < 10000000)
+        {
+            cout << " El numero que ingreso no es valido, vuelva a intentarlo ";
+            cin >> numcel;
+        }
+
+        else
+        {
+            valido = true;
+        }
+
+    } while (valido == false);
+}
+
+string confirmacion(string correo, string confi_correo)
+{
+    bool validar = false;
+
+    do
+    {
+        if (correo == confi_correo)
+        {
+            cout << "Su correo ha sido validado, pronto le agendaremos la cita " << endl;
+            validar = true;
+        }
+
+        else
+        {
+            cout << " El correo no coincide, vuelva a intentarlo " << endl;
+            cout << " Ingrese su correo para agendarle una cita ";
+            cin >> correo;
+            cout << " Vuelva a ingresar su correo ";
+            cin >> confi_correo;
+        }
+
+    } while (validar == false);
 }
